bc5480f570648008e3eaa1220f6fada890d67ef4
luisfvsanchez <luisfvsanchez@hotmail.com>
2020-09-29 09:18:57 -0300

    Updates

---
+++ precache-manifest.8b24d2c02e7b73441a8352107a868ee5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca99b043b23f26eb71957a9fa272eea",
+    "url": "/frontend-exam/index.html"
+  },
+  {
+    "revision": "31a7edc9319f8b439660",
+    "url": "/frontend-exam/static/css/main.dd1319ac.chunk.css"
+  },
+  {
+    "revision": "90775ccc06da1b41ab3c",
+    "url": "/frontend-exam/static/js/2.cc0e0514.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/frontend-exam/static/js/2.cc0e0514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a7edc9319f8b439660",
+    "url": "/frontend-exam/static/js/main.508e1e24.chunk.js"
+  },
+  {
+    "revision": "e5e3e849a2113f4f6832",
+    "url": "/frontend-exam/static/js/runtime-main.c55cc90b.js"
+  },
+  {
+    "revision": "5b85f90bb8d97e67e9f7f9851042be29",
+    "url": "/frontend-exam/static/media/background.5b85f90b.png"
+  },
+  {
+    "revision": "e10411ccedbf339cc64e915868709295",
+    "url": "/frontend-exam/static/media/loader.e10411cc.svg"
+  },
+  {
+    "revision": "42dbc4d19f04768b92d32475c29caf8e",
+    "url": "/frontend-exam/static/media/logo_dark.42dbc4d1.png"
+  }
+]);
